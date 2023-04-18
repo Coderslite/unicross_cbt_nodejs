3387a55bc0442e6e3481726dd94fe4e77bb9b5c6
Coderslite <abrahamgreatebele@gmail.com>
2023-04-18 17:48:52 +0100

    option type changed

--- models/questionSchema.js
+++ models/questionSchema.js
@@ -7,7 +7,7 @@ import { Schema } from "mongoose";
 const questionModel = new Schema({
     question :{type:String, default:''},
     type:{type:String,default:'obj'},
-    options : {type:Array, default:[]},
+    options : {type:String, default:''},
     answer: {type:String, default:''},
     createdAt: {type:Date,default:Date.now},
 },{ versionKey: false})
