dfbc499b228146ce5cc31e6549e94e844e8ce809
Coderslite <abrahamgreatebele@gmail.com>
2023-04-18 15:46:12 +0100

    url encoded data

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(morgan('tiny'));
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 config();
 
 
